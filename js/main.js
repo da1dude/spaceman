1f83147581c78f7e1ca79c55797f04afbb9d238d
Paul K <pkerschieter@gmail.com>
2023-11-29 15:57:03 -0600

    Added renderHiddenWord

--- js/main.js
+++ js/main.js
@@ -26,14 +26,48 @@ const wordDB = [
 
 
 /*------ cached element references ------*/
+//grabbing the hidden word element - HTML
+const hiddenWordEl = document.getElementById('hword');
+//console.log("this is hword value: ", hiddenWordEl)
 
 
 /*------ App's initial state variables ------*/
 let winner
-let guesses
+let wrongGuesses
 let hiddenWord
+let letterSpace
+
 
 /*------ functions ------*/
+function init() {
+    winner = null
+    guesses = 0
+    //select the hidden word from wordDB
+    hiddenWord = wordDB[Math.floor(Math.random() * wordDB.length)];
+    console.log("hidden word is: ", hiddenWord)
+    //eventually call render   
+    render() 
+}
 
+init()
+
+function renderHiddenWord() {
+    for (let i = 0; i < hiddenWord.length; i++) {
+        //for each letter in hiddenWord create a span to hold
+        //the hiddenWord spaces in the hword element
+		letterSpace = document.createElement('span');
+		letterSpace.innerText = '_____ ';
+        hword.appendChild(letterSpace);
+	}
+}
+
+function render(){
+    renderHiddenWord()
+}
+
+function handleChoice() {
+
+}
 
 /*------ event listeners ------*/
+document.querySelector('#reset').addEventListener('click', init)
